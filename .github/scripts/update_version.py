dc5f9f28a3523c3ea183711bbea8fbc805724360
AlexandreOkita <xande.okita@gmail.com>
2021-03-16 19:38:27 -0300

    add update-version job

--- .github/scripts/update_version.py
+++ .github/scripts/update_version.py
@@ -1,13 +1,14 @@
-import subprocess
 import re
-import fileinput
+import pathlib
 
-with open('../../setup.py') as f:
+current_path = str(pathlib.Path(__file__).parent.absolute())
+
+with open(current_path+'/../../setup.py') as f:
     lines = f.readlines()
     version_line = lines[0]
     version = float(''.join(re.findall("'(.*?)'", version_line)))
 
 lines[0] = f"VERSION = '{(version + 0.01):.2f}'\n"
 
-with open('../../setup.py', 'w') as f:
+with open(current_path+'/../../setup.py', 'w') as f:
     f.writelines(lines)
